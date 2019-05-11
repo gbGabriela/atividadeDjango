4e4cb08a409bfbbb135aa528b5a77697373bf400
gabriela <gabi_goncalves99@hotmail.com>
2019-05-11 00:37:30 -0300

    aula de 1105

--- Atividade/metodoAvaliacao/urls.py
+++ Atividade/metodoAvaliacao/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from metodoAvaliacao.views import IndexView
+from metodoAvaliacao.views import *
 
 urlpatterns = [
 	path('', IndexView.as_view(), name="index"),
+	path('curriculo/', CurriculoView.as_view(), name="curriculo"),
+	path('sobre/', SobreView.as_view(), name="sobre"),
+	path('cadastro/', CadastroView.as_view(), name="cadastro"),
 ]
--- Atividade/metodoAvaliacao/views.py
+++ Atividade/metodoAvaliacao/views.py
@@ -3,3 +3,12 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+class CurriculoView(TemplateView):
+    template_name = "curriculo.html"
+
+class SobreView(TemplateView):
+    template_name = "sobre.html"
+
+class CadastroView(TemplateView):
+    template_name = "cadastro.html"
